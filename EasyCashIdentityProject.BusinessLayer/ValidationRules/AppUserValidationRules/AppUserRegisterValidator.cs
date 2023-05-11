14f03d2430b04b389254ffacaf7f76a782cebe9e
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-11 23:21:55 +0300

    Fluent validation added.

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator() 
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez.");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanı adı alanı boş geçilemez.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("E-mail alanı boş geçilemez.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez.");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez.");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("En fazla 30 karakter giriş yapabilirsiniz.");
+            RuleFor(x => x.Name).MinimumLength(30).WithMessage("En az 2 karakter giriş yapabilirsiniz.");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız eşleşmiyor.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz.");
+        }
+    }
+}
