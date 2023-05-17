f9205ebe8c71ca3039f8250c7217a49e42242322
AHMET CAN <ahmetcan9816@gmail.com>
2023-05-17 16:06:00 +0300

    Confirmation code and mail info tried. #15

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,7 +1,11 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Net.Mail;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
@@ -24,6 +28,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         {
             if(ModelState.IsValid)
             {
+                Random random = new Random();
+                int code = random.Next(100000, 1000000);
                 AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
@@ -32,11 +38,31 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     Email = appUserRegisterDto.Email,
                     City = "Bursa",
                     District = "MKP",
-                    ImageUrl = "test"
+                    ImageUrl = "test",
+                    PhoneNumber = "+905346199695",
+                    ConfirmCode = code
                 };
                 var result = await _userManager.CreateAsync(appUser,appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
+                    //MimeMessage mimeMessage = new MimeMessage();
+                    //MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "ahmetcan9816@gmail.com");
+                    //MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    //mimeMessage.From.Add(mailboxAddressFrom);
+                    //mimeMessage.To.Add(mailboxAddressTo);
+
+                    //var bodyBuilder = new BodyBuilder();
+                    //bodyBuilder.TextBody = "Kayıt işlemi gerçekleştirmek için onay kodunuz:" + code;
+                    //mimeMessage.Body = bodyBuilder.ToMessageBody();
+                    //mimeMessage.Subject = "Easy Cash Onay Kodu";
+                    //SmtpClient client = new SmtpClient();
+                    //client.Connect("smtp.gmail.com",587,false);
+                    //client.Authenticate("ahmetcan9816@gmail.com", "wzscasqhlrxiebbl");
+                    //client.Send(mimeMessage);
+                    //client.Disconnect(true);
+
+                    TempData["Mail"] = appUserRegisterDto.Email;
                     return RedirectToAction("Index","ConfirmMail");
                 }
                 else
@@ -51,3 +77,4 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         }
     }
 }
+
